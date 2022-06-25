0372e855767c63c73e741ae8aae1adbeac075b2d
Elom ESSAN <elomessan@Eloms-iMac.local>
2022-06-25 09:40:42 +0200

    iteration 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import IdCard from './components/IdCard';
 import IdList from './components/IdList';
 import Greetings from './components/Greetings';
+import Random from './components/Random';
 
 function App() {
   return <div className="App">
@@ -17,6 +18,10 @@ function App() {
 
     <Greetings lang={"de"}>Ludwig</Greetings>
     <Greetings lang={"fr"}>Francois</Greetings>
+
+{/*Iteration 3*/} 
+    <Random min={1} max={6}/>
+    <Random min={1} max={100}/>
     
   </div>;
 }
